e002c66413daf25c3e4dc2dc6f6d5f4563db1625
Scott Hamilton <scott.hamilton@det.nsw.edu.au>
2014-02-03 18:02:53 +1100

    socket.io

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,28 +1,32 @@
 'use strict';
 
 /* Controllers */
+var app = angular.module('myApp.controllers', ['ngResource']);
 
-angular.module('myApp.controllers', []).
-        controller('MyCtrl1', ['$scope', function($scope) {
+app.controller('MyCtrl1', ['$scope', function($scope) {
 
-            }])
-        .controller('MyCtrl2', [function() {
+    }]);
 
-            }])
-        .controller('switcher', ['$scope', function($scope) {
-                $scope.currentPgmBtn = 1;
-                $scope.currentPreviewBtn = 0;
-                $scope.programButtons = [{idx: 0, class: "switchOFF", id: "CAM1"}, {idx: 1, class: "switchON", id: "CAM2"}, {idx: 2, class: "switchOFF", id: "CAM3"}, {idx: 3, class: "switchOFF", id: "VTR1"}, {idx: 4, class: "switchOFF", id: "VTR2"}];
-                $scope.previewButtons = [{idx: 0, class: "switchON", id: "CAM1"}, {idx: 1, class: "switchOFF", id: "CAM2"}, {idx: 2, class: "switchOFF", id: "CAM3"}, {idx: 3, class: "switchOFF", id: "VTR1"}, {idx: 4, class: "switchOFF", id: "VTR2"}];
+app.controller('MyCtrl2', [function() {
 
-                $scope.cutPgm = function(btn) {
-                    $scope.programButtons[$scope.currentPgmBtn].class = "switchOFF";
-                    $scope.programButtons[btn].class = "switchON";
-                    $scope.currentPgmBtn = btn;
-                };
-                $scope.cutPreview = function(btn) {
-                    $scope.previewButtons[$scope.currentPreviewBtn].class = "switchOFF";
-                    $scope.previewButtons[btn].class = "switchON";
-                    $scope.currentPreviewBtn = btn;
-                };
-            }]);
+    }]);
+
+app.controller('switcher', ['$scope', 'socket', function($scope, socket) {
+        $scope.currentPgmBtn = 1;
+        $scope.currentPreviewBtn = 0;
+        $scope.programButtons = [{idx: 0, class: "switchOFF", id: "CAM1"}, {idx: 1, class: "switchON", id: "CAM2"}, {idx: 2, class: "switchOFF", id: "CAM3"}, {idx: 3, class: "switchOFF", id: "VTR1"}, {idx: 4, class: "switchOFF", id: "VTR2"}];
+        $scope.previewButtons = [{idx: 0, class: "switchON", id: "CAM1"}, {idx: 1, class: "switchOFF", id: "CAM2"}, {idx: 2, class: "switchOFF", id: "CAM3"}, {idx: 3, class: "switchOFF", id: "VTR1"}, {idx: 4, class: "switchOFF", id: "VTR2"}];
+
+        $scope.cutPgm = function(btn) {
+            socket.emit("cutPgm", btn);
+            $scope.programButtons[$scope.currentPgmBtn].class = "switchOFF";
+            $scope.programButtons[btn].class = "switchON";
+            $scope.currentPgmBtn = btn;
+        };
+        $scope.cutPreview = function(btn) {
+            socket.emit("cutPreview", btn);
+            $scope.previewButtons[$scope.currentPreviewBtn].class = "switchOFF";
+            $scope.previewButtons[btn].class = "switchON";
+            $scope.currentPreviewBtn = btn;
+        };
+    }]);
--- server.js
+++ server.js
@@ -21,31 +21,53 @@ var path = require('path');
 
 var async = require('async'), socketio = require('socket.io');
 var express = require('express'), app = express(), server = http.createServer(app);
+var io = require('socket.io').listen(server);
 var melted_node = require('melted-node');
 
 //var mlt = new melted_node('ctl.journostream.org.au', 5250);
 
 var viewEngine = 'jade'; // modify for your view engine
 // Configuration
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', viewEngine);
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
+app.configure(function() {
+    app.set('views', __dirname + '/views');
+    app.set('view engine', viewEngine);
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(__dirname + '/public'));
 });
 /*************
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
-app.configure('production', function(){
-  app.use(express.errorHandler());
-});
-*******/
+ app.configure('development', function(){
+ app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+ });
+ app.configure('production', function(){
+ app.use(express.errorHandler());
+ });
+ *******/
 
 server.listen(process.env.PORT);
 var addr = server.address().address;
 console.log('Started listening on: '.concat(addr).concat(':').concat(process.env.PORT));
 
+
+io.sockets.on('connection', function(socket) {
+    console.log("Got connection...");
+    socket.on("cutPGM", function(data) {
+        console.log("cutPGM: "+data);
+        //socket.broadcast.emit('onNoteCreated', data);
+    });
+
+    socket.on("cutPreview", function(data) {
+        console.log("cutPreview: "+data);
+        //socket.broadcast.emit('onNoteUpdated', data);
+    });
+
+    socket.on('mix', function(data) {
+        socket.broadcast.emit('onNoteDeleted', data);
+    });
+
+    socket.on('cut', function(data) {
+        socket.broadcast.emit('onNoteMoved', data);
+    });
+});
 
