a4b163d51b5b79672e3d109b5f8a08a493508cd8
Scott Hamilton <scott.hamilton@popplanet.biz>
2014-03-24 10:30:01 +1100

    rename view names

--- public/js/app.js
+++ public/js/app.js
@@ -10,8 +10,8 @@ angular.module('myApp', [
   'myApp.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'switcher'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
+  $routeProvider.when('/pres', {templateUrl: 'partials/partial1.html', controller: 'switcher'});
+  $routeProvider.when('/journo', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
   $routeProvider.when('/ingest', {templateUrl: 'partials/ingest.html', controller: 'MyCtrl1'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.otherwise({redirectTo: '/journo'});
 }]);
